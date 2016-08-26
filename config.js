ab72b7430028d9ec803804285c9f58af5af53fa4
Dennis Wambua <dennis.wambua@gmail.com>
2016-08-26 15:07:19 +0300

    first commit
    Signed-off-by: Dennis Wambua <dennis.wambua@gmail.com>

---
+++ config.js
@@ -0,0 +1,9 @@
+// The necessary configuration for your server
+// And the new redirect path for the OAuth flow
+module.exports = {
+ 'HOSTPATH': 'http://localhost',
+ 'PORT': 3333,
+ 'CLIENT_ID': 'hoHoaHf7oQfVabWSuktnrXfAIIGXXVA74dmIGpgw',
+ 'CLIENT_SECRET': 'Igi7hLG04gQsFpiS3A9kQafjANOkhl02LJJGKuYCJIT9ZXYPmO41A41XqcbyPVOrPK8rSVqLXYWE7aEXnC9Jk3l6w0jDLDS2VTdnmlnzsJBXHRjqCAeFFfUrIY39ukmn',
+ 'REDIRECT_PATH': '/redirect'
+};
